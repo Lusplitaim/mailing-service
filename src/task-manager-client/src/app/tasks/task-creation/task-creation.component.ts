86d955622e6007f4ef137d3df44539e55ce6fed9
Kirill Loban <Foolman.ru@yandex.ru>
2022-11-17 22:40:49 +0300

    add form controllers to Task Creation Component

--- src/task-manager-client/src/app/tasks/task-creation/task-creation.component.ts
+++ src/task-manager-client/src/app/tasks/task-creation/task-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-creation',
@@ -11,9 +12,35 @@ export class TaskCreationComponent implements OnInit {
 
   activeApiTab: string = this.services[0];
 
-  constructor() { }
+  taskForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.taskForm = this.formBuilder.group({
+      email: [null, [ Validators.required, Validators.email ] ],
+      api:  [this.activeApiTab, [ Validators.required ] ],
+      minutes: [[], [ Validators.required ] ],
+      hours: [[], [ Validators.required ] ],
+      days: [[], [ Validators.required ] ],
+      months: [[], [ Validators.required ] ],
+      weekdays: [[], [ Validators.required ] ],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  createTask() {
+    let taskDetails: any = {};
+    taskDetails.email = this.taskForm.value['email'];
+    taskDetails.api = this.taskForm.value['api'];
+
+    taskDetails.minutes = this.taskForm.value['minutes'].toString();
+    taskDetails.hours = this.taskForm.value['hours'].toString();
+    taskDetails.days = this.taskForm.value['days'].toString();
+    taskDetails.months = this.taskForm.value['months'].toString();
+    taskDetails.weekdays = this.taskForm.value['weekdays'].toString();
+
+    console.log(taskDetails);
   }
 
 }
