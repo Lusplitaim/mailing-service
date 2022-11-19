44b4796567064307aef6c4c87b4fb65b413674b4
Kira <Foolman.ru@yandex.ru>
2022-11-19 22:23:17 +0300

    add toast service and component

---
+++ src/task-manager-client/src/app/services/toast.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+export interface ToastInfo {
+  header: string;
+  body: string;
+  delay?: number;
+  classname?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+  toasts: ToastInfo[] = [];
+
+  show(header: string, body: string) {
+    this.toasts.push({ header, body, delay: 4000 });
+  }
+
+  showError(error: string) {
+    this.toasts.push({ header: "Error", body: error, delay: 4000, classname: 'bg-danger text-light' });
+	}
+
+  remove(toast: ToastInfo) {
+    this.toasts = this.toasts.filter(t => t != toast);
+  }
+
+  constructor() { }
+
+
+}
